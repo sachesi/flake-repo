7dbe5bc13140b9936aa2c32fb1d645bee07b2e23
sachesi <sachesi.bb.passp@gmail.com>
2025-05-24 00:09:54 +0300

    update custom-icons to 0.1-3, fix package homepage

--- pkgs/custom-icons/package.nix
+++ pkgs/custom-icons/package.nix
@@ -3,13 +3,13 @@
 let
   src = pkgs.fetchgit {
     url = "ssh://git@github.com:sachesi/custom-icons.git";
-    rev = "11dc70hin9xdgvi0v3qby2lrl1bpgig4mmf64hzc291q5gpsnfna";
-    sha256 = "sha256-yjqr7ys4JME+JMbVSl58dwWaqfALjw3ifq0nGyE4rIU=";
+    rev = "0jfinvnyg1q3rp5sl56w0119ypdx5ac5chhp22c6rfwqz0c35mff";
+    sha256 = "sha256-ztUyGPiYu2yYEBdCVpgqvV2fQgDcFKrLzQOH5+220Uk=";
   };
 in
 pkgs.stdenv.mkDerivation {
   pname = "custom-icons";
-  version = "0.1-2";
+  version = "0.1-3";
   src = src;
   installPhase = ''
     runHook preInstall
@@ -22,7 +22,7 @@ pkgs.stdenv.mkDerivation {
 
   meta = with pkgs.lib; {
     description = "Custom app icons for my apps";
-    homepage = "https://github.com/sachesi/desktop-thumbnailer/";
+    homepage = "https://github.com/sachesi/custom-icons/";
     license = pkgs.lib.licenses.free;
     maintainers = [
       {
