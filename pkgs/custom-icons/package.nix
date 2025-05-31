b1323a7181107c29879b65b90ca526cdaf86edcb
sachesi <sachesi.bb.passp@gmail.com>
2025-05-31 23:07:26 +0300

    update custom-icons to 0.1-4

--- pkgs/custom-icons/package.nix
+++ pkgs/custom-icons/package.nix
@@ -3,13 +3,13 @@
 let
   src = pkgs.fetchgit {
     url = "ssh://git@github.com:sachesi/custom-icons.git";
-    rev = "0jfinvnyg1q3rp5sl56w0119ypdx5ac5chhp22c6rfwqz0c35mff";
-    sha256 = "sha256-ztUyGPiYu2yYEBdCVpgqvV2fQgDcFKrLzQOH5+220Uk=";
+    rev = "02iz2436ffvsjnp7masl8g9f5mkjply83260k5xa92n8wl4bcwb7";
+    sha256 = "sha256-Z3G2COXIiqR6mcCIgTy9ctbi0kNUq3qulXo7ZwYRPwo=";
   };
 in
 pkgs.stdenv.mkDerivation {
   pname = "custom-icons";
-  version = "0.1-3";
+  version = "0.1-4";
   src = src;
   installPhase = ''
     runHook preInstall
